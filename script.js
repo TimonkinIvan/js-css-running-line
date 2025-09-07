14e22d2f3077453bab88780a3aace5f6d0f6bc0c
TimonkinIvan <timonkin.ivan@gmail.com>
2025-09-07 13:28:38 +0700

    chore: final fix with VS Code

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
   const contentContainer = document.querySelector('.marquee-content');
-  
   const originalText = 'одежда как признание';
   const linkUrl = 'https://lovelinst.ru/';
-  
   let repeatedContent = '';
   
-  // Дублируем текст 15 раз, чтобы строка была очень длинной
-  for (let i = 0; i < 15; i++) {
+  for (let i = 0; i < 20; i++) {
     repeatedContent += `<a href="${linkUrl}">${originalText}</a>`;
   }
   
-  // Добавляем содержимое в контейнер дважды для бесшовного зацикливания
-  contentContainer.innerHTML = repeatedContent + repeatedContent;
+  contentContainer.innerHTML = repeatedContent;
 });
